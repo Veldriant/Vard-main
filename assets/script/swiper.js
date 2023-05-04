d02251b6dda18a67ac847b6aa5f850536f523c28
Veldriant <65666734+Veldriant@users.noreply.github.com>
2023-05-04 22:00:16 +0300

    first fix

--- assets/script/swiper.js
+++ assets/script/swiper.js
@@ -1,20 +1,28 @@
 var swiper = new Swiper(".swiperHome-main", {
-      navigation: {
-        nextEl: ".swiper-button-next" ,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+var swiper = new Swiper(".swiperHome-cardMiniatur", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  spaceBetween: 30,
+});
+var swiper = new Swiper(".homeNew", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  navigation: {
+        nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
-      },
-      loop:true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
- var swiper = new Swiper(".swiperHome-popular", {
-      navigation: {
-        nextEl: ".swiper-button-next" ,
-        prevEl: ".swiper-button-prev",
-      },
-      slidesPerView: 4,
-      spaceBetween: 30,
-      centeredSlides: true,
+  },
     });
