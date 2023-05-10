75b8e982f69f6ac6059ce8b99061293dd3817477
Veldriant <65666734+Veldriant@users.noreply.github.com>
2023-05-10 17:24:59 +0300

    fix

--- assets/script/swiper.js
+++ assets/script/swiper.js
@@ -1,9 +1,10 @@
 var swiper = new Swiper(".swiperHome-main", {
+  slidesPerView: 1,
+  loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  loop: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
